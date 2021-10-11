88b4517ecb819c39ed13203c37318b55d1dc42ae
DribasRV <dribas.rv@phystech.edu>
2021-10-11 11:01:13 +0300

    Added a second type of target with its own shape and its own specific character of movement and number of points.

--- lab6/main.py
+++ lab6/main.py
@@ -20,32 +20,61 @@ COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 # 0 - x, 1 - y, 2 - r, 3 - v_x, 4 - v_y
 balls = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+targets = [[0, 0, 0, 0, 0, 0]]
 
 
 score = 0
 
 
-def new_ball(ball_n):
-    balls[ball_n][0] = randint(100, 700)
-    balls[ball_n][1] = randint(100, 500)
-    balls[ball_n][2] = randint(30, 50)
-    balls[ball_n][3] = randint(1, 50)
-    balls[ball_n][4] = randint(1, 50)
-    balls[ball_n][5] = COLORS[randint(0, 5)]
+def new_ball(i):
+    balls[i][0] = randint(100, 700)
+    balls[i][1] = randint(100, 500)
+    balls[i][2] = randint(30, 50)
+    balls[i][3] = randint(1, 50)
+    balls[i][4] = randint(1, 50)
+    balls[i][5] = COLORS[randint(0, 5)]
 
 
-def draw_ball(ball_n):
-    circle(screen, balls[ball_n][5], (balls[ball_n][0], balls[ball_n][1]), balls[ball_n][2])
+def draw_ball(i):
+    circle(screen, balls[i][5], (balls[i][0], balls[i][1]), balls[i][2])
 
 
-def move_ball(ball_n):
-    r = balls[ball_n][2]
-    balls[ball_n][0] += balls[ball_n][3]
-    balls[ball_n][1] += balls[ball_n][4]
-    if (balls[ball_n][0] <= r) or (balls[ball_n][0] >= screen_width - r):
-        balls[ball_n][3] *= -1
-    if (balls[ball_n][1] <= r) or (balls[ball_n][1] >= screen_heigh - r):
-        balls[ball_n][4] *= -1
+def move_ball(i):
+    r = balls[i][2]
+    balls[i][0] += balls[i][3]
+    balls[i][1] += balls[i][4]
+    if (balls[i][0] <= r) or (balls[i][0] >= screen_width - r):
+        balls[i][3] *= -1
+    if (balls[i][1] <= r) or (balls[i][1] >= screen_heigh - r):
+        balls[i][4] *= -1
+
+
+def new_target(i):
+    targets[i][0] = randint(100, 700)
+    targets[i][1] = randint(100, 500)
+    targets[i][2] = randint(10, 20)
+    targets[i][3] = randint(30, 75)
+    targets[i][4] = randint(30, 75)
+    targets[i][5] = COLORS[randint(0, 5)]
+
+
+def draw_target(i):
+    rect(screen, targets[i][5], (targets[i][0], targets[i][1], targets[i][2], targets[i][2]))
+
+
+def move_target(i):
+    r = targets[i][2]
+    targets[i][0] += targets[i][3]
+    targets[i][1] += targets[i][4]
+    if (targets[i][0] <= r) or (targets[i][0] >= screen_width - r):
+        targets[i][3] *= -1
+    if (targets[i][1] <= r) or (targets[i][1] >= screen_heigh - r):
+        targets[i][4] *= -1
+
+
+def turn_target(i):
+    targets[i][3] *= -1
+    targets[i][4] *= -1
 
 
 def click(event):
@@ -54,6 +83,12 @@ def click(event):
         if (event.pos[0] - balls[i][0]) ** 2 + (event.pos[1] - balls[i][1]) ** 2 <= balls[i][2] ** 2:
             score += 1
             new_ball(i)
+    for i in range(len(targets)):
+        if ((event.pos[0] - targets[i][0]) ** 2 <= targets[i][2] ** 2) or ((event.pos[1] - targets[i][1]) ** 2 <= targets[i][2] ** 2):
+            score += 5
+            new_target(i)
+        elif ((event.pos[0] - targets[i][0]) ** 2 <= (targets[i][2] * 5) ** 2) or ((event.pos[1] - targets[i][1]) ** 2 <= (targets[i][2] * 5) ** 2):
+            turn_target(i)
 
 
 pygame.display.update()
@@ -61,19 +96,25 @@ clock = pygame.time.Clock()
 for i in range(len(balls)):
     new_ball(i)
     draw_ball(i)
+for i in range(len(targets)):
+    new_target(i)
+    draw_target(i)
 finished = False
 
 while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print(score)
+            print('Score:', score)
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             click(event)
     for i in range(len(balls)):
         draw_ball(i)
         move_ball(i)
+    for i in range(len(targets)):
+        draw_target(i)
+        move_target(i)
     pygame.display.update()
     screen.fill(BLACK)
 
