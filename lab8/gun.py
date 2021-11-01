97da75c6016a501ffa1d101d51a7fbf00fec90ce
DribasRV <dribas.rv@phystech.edu>
2021-11-01 13:25:50 +0300

    Added many targets and deleting of balls after they kill all targets.

--- lab8/gun.py
+++ lab8/gun.py
@@ -167,20 +167,44 @@ class Target:
     screen = pygame.Surface((0, 0))
     x = 0
     y = 0
+    vx = 0
+    vy = 0
     r = 1
     color = RED
 
-    def __init__(self, screen):
-        """ Инициализация новой цели. """
+    def refresh(self, screen):
         self.screen = screen
+        self.live = 1
         self.x = randint(600, 780)
         self.y = randint(300, 550)
+        self.vx = randint(-5, 5)
+        self.vy = randint(-5, 5)
         self.r = randint(2, 50)
         self.color = RED
+
+    def __init__(self, screen):
+        """ Инициализация новой цели. """
+        self.refresh(screen)
 
     def hit(self, points=1):
         """Попадание шарика в цель."""
         self.points += points
+
+    def move(self):
+        self.x += self.vx
+        self.y += self.vy
+        if self.x < 0:
+            self.vx *= -1
+            self.x = 0
+        elif self.x > WIDTH:
+            self.vx *= -1
+            self.x = WIDTH
+        if self.y < 0:
+            self.vy *= -1
+            self.y = 0
+        elif self.y > HEIGHT:
+            self.vy *= -1
+            self.y = HEIGHT
 
     def draw(self):
         pygame.draw.circle(
@@ -198,13 +222,18 @@ balls = []
 
 clock = pygame.time.Clock()
 gun = Gun(screen)
-target = Target(screen)
+targets = []
+targets_number = 2
+for _ in range(targets_number):
+    targets.append(Target(screen))
 finished = False
 
 while not finished:
     screen.fill(WHITE)
     gun.draw()
-    target.draw()
+    for t in targets:
+        if t.live:
+            t.draw()
     for b in balls:
         b.draw()
     pygame.display.update()
@@ -222,10 +251,23 @@ while not finished:
 
     for b in balls:
         b.move()
-        if b.hittest(target) and target.live:
-            target.live = 0
-            target.hit()
-            target = Target(screen)
+        for t in targets:
+            if b.hittest(t) and t.live:
+                t.live = 0
+                t.hit()
+
+    hitted = True
+    for t in targets:
+        if t.live == 1:
+            hitted = False
+    if hitted:
+        balls = []
+        for t in targets:
+            t.refresh(screen)
+
+    for t in targets:
+        t.move()
+
     gun.power_up()
 
 pygame.quit()
